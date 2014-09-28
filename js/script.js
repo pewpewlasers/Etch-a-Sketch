8244e7872f705d7e82eccc2f3aa2294bcbbec185
Garrett Olson <garrettdolson@gmail.com>
2014-09-28 11:33:26 -0700

    Completed project

--- js/script.js
+++ js/script.js
@@ -1,30 +1,55 @@
 $(document).ready(function() {
+	$('#reset').on('click', function() {
+		setUpPage();
+	});
+	setUpPage();
+});
+
+function setUpPage() {	
+
+	var numSquaresPerRow;
+	var acceptableNumber = false;
+
+	while(!acceptableNumber) {
+		var response = prompt('How many squares per row?');
+		if((response > 200) || (response < 3)) {
+			alert("Please choose a number greater than 3 and less than 200.");
+		}
+		else {
+			acceptableNumber = true;
+			numSquaresPerRow = response;
+		}	
+	}	
+
+	clearBoard();
+
 	var windowWidth = $(window).width();
 	var windowHeight = $(window).height();
+	var wrapperTargetWidth = windowWidth * .70;
+	var wrapperTargetHeight = windowHeight * .70;
+	var squareSize = wrapperTargetWidth / numSquaresPerRow;
+	var numRows = Math.floor(wrapperTargetHeight / squareSize);
 
-	$('#wrapper').css({'height': (windowHeight * .80) + 'px', 'width': (windowWidth * .70) + 'px'});
-	$('#main-wrapper').css('width', (windowWidth * .70) + 'px');
+	$('header').fadeIn(900);
 
-	var numSquaresPerRow = 100;	
-	var squareSidePx = ($('#wrapper').width() / numSquaresPerRow);	
-	var numRows = ($('#wrapper').height() / squareSidePx);
-	//alert(numRows);
-	//$('.square').css({'height': squareSize, 'width': squareSize});
-	populateSquares(numSquaresPerRow, numRows);
+	$('#main-wrapper').css('width', (squareSize * numSquaresPerRow) + 'px');
+	$('#wrapper').css({'height': (squareSize * numRows) + 'px', 'width': (squareSize * numSquaresPerRow) + 'px'});
+
+	populateSquares(numSquaresPerRow, numRows, squareSize);
 
 	$('.square').on('mouseenter', function() {
 		$(this).addClass('fill');
 	});
+}
 
-});
-
-function populateSquares(numSquaresPerRow, numRows) {
-		var squareSize = ($('#wrapper').width() / numSquaresPerRow) +'px';	
-	
+function populateSquares(numSquaresPerRow, numRows, squareSize) {
 		for(var i = 0; i < (numSquaresPerRow * numRows); i++) {
-			//console.log("TERST");
 			$('#wrapper').append('<div class=\'square\'></div>'); 
 		}		
 		$('.square').css({'height': squareSize, 'width': squareSize});
-	}
+}
+
+function clearBoard() {
+	$('.square').remove();
+}
 
